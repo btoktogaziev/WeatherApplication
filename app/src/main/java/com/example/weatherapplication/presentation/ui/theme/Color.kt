305bebdc4cf1c0e7e1fb7eb0bf80d99f222d81dc
btoktogaziev <btoktogaziev@nurtelecom.kg>
2025-11-17 16:37:04 +0600

    Color and styles

---
+++ app/src/main/java/com/example/weatherapplication/presentation/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.example.weatherapplication.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val SkyBlue = Color(0xA36FD2FF)
+val TransparentWhite = Color(0x27FFFFFF)
+val ProgressBarBlue = Color(0xFF83D9FF)
+val ProgressBarGreen = Color(0xFF00FFA6)
+val ProgressBarCold = Color(0xFF00A6FF)
+val ProgressBarHot = Color(0xFFFF7700)
